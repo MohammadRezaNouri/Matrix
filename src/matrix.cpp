0dde8172abcaa7c5decda784b40ed7ce77e9b0c9
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-04-08 03:47:41 +0430

    Implementing the Initialize in matrix.cpp

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -9,9 +9,7 @@ Matrix::Matrix(size_t row_col)
 {
     _mtx = new float *[row_col];
     for (unsigned long int i = 0; i < row_col; i++)
-    {
         _mtx[i] = new float[row_col];
-    }
     _rowsize = row_col;
     _colsize = row_col;
 }
@@ -20,13 +18,18 @@ Matrix::Matrix(size_t row, size_t col)
 {
     _mtx = new float *[row];
     for (unsigned long int i = 0; i < row; i++)
-    {
         _mtx[i] = new float[col];
-    }
     _rowsize = row;
     _colsize = col;
 }
 
 Matrix::~Matrix()
 {
+}
+
+void Matrix::Initialize()
+{
+    for (unsigned long int i = 0; i < _rowsize; i++)
+        for (unsigned long int j = 0; j < _colsize; j++)
+            cin >> _mtx[i][j];
 }
