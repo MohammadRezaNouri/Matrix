5a1e6a36d37721d1febb42e4379b4c39ceb56be8
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-04-08 03:59:50 +0430

    Implementing the Transposed in matrix.cpp

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -49,3 +49,34 @@ void Matrix::Display(size_t T1, size_t T2, size_t T3, size_t T4) const
             cout << _mtx[i][j] << "\t";
         cout << endl;
 }
+
+void Matrix::Transposed()
+{
+    unsigned long int temp;
+    float **tmp = new float *[_colsize];
+    for (unsigned long int Q = 0; Q < _colsize; Q++)
+        tmp[Q] = new float[_rowsize];
+    for (unsigned long int i = 0; i < _colsize; i++)
+        for (unsigned long int j = 0; j < _rowsize; j++)
+            tmp[i][j] = _mtx[j][i];
+    Deallocate();
+    _mtx = new float *[_colsize];
+    for (unsigned long int MN = 0; MN < _colsize; MN++)
+        _mtx[MN] = new float[_rowsize];
+    temp = _rowsize;
+    _rowsize = _colsize;
+    _colsize = temp;
+    for (unsigned long int M = 0; M < _rowsize; M++)
+        for (unsigned long int N = 0; N < _colsize; N++)
+            _mtx[M][N] = tmp[M][N];
+    cout << "Do you want to see the modified matrix?(no=0, yes=1)" << endl;
+    cin >> temp;
+    if (temp == 1)
+    {
+        cout << "Matrix Converted :" << endl;
+        Display();
+    }
+    for (unsigned long int i = 0; i < _rowsize; i++)
+        delete[] tmp[i];
+    delete[] tmp;
+}
