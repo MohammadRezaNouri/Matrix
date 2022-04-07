30e1c2dd2e0973e077af2dda8b31d1ffb6e9d7d2
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-04-08 03:24:12 +0430

    Definition of class matrix in matrix.h

--- includes/matrix.h
+++ includes/matrix.h
@@ -1,6 +1,27 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+class Matrix
+{
+public:
+    Matrix(size_t);
+    Matrix(size_t, size_t);
+    ~Matrix();
+    void Initialize();
+    void Display() const;
+    void Display(size_t, size_t, size_t, size_t) const;
+    void Transposed();
+    void Add(Matrix);
+    void Sub(Matrix);
+    void Deallocate();
+    void special(size_t, size_t, float);
+    void Power(size_t);
+    void Copy(Matrix);
 
+private:
+    float **_mtx;
+    size_t _rowsize;
+    size_t _colsize;
+};
 
 #endif
