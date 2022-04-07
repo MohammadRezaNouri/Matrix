f0438d4653893bfcb723438b8936161e133aea4b
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-04-08 03:30:02 +0430

    Implementing the constructor 2 in matrix.cpp

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -15,3 +15,14 @@ Matrix::Matrix(size_t row_col)
     _rowsize = row_col;
     _colsize = row_col;
 }
+
+Matrix::Matrix(size_t row, size_t col)
+{
+    _mtx = new float *[row];
+    for (unsigned long int i = 0; i < row; i++)
+    {
+        _mtx[i] = new float[col];
+    }
+    _rowsize = row;
+    _colsize = col;
+}
