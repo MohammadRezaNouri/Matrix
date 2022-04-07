3f2f45a08aaf69d8dceb8b345e75012b6dc5525f
MohammadRezaNouri <MohammadRezaNouri01@Gmail.Com>
2022-04-08 03:29:49 +0430

    Implementing the constructor 1 in matrix.cpp

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -4,3 +4,14 @@
 #include <iomanip>
 
 using namespace std;
+
+Matrix::Matrix(size_t row_col)
+{
+    _mtx = new float *[row_col];
+    for (unsigned long int i = 0; i < row_col; i++)
+    {
+        _mtx[i] = new float[row_col];
+    }
+    _rowsize = row_col;
+    _colsize = row_col;
+}
